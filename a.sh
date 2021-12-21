26d303c9c034a6977ff289a18f1e469d7298a6e5
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-21 17:14:49 +0000

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,11 @@
+#export GPU_MAX_HEAP_SIZE=100
+#export GPU_MAX_USE_SYNC_OBJECTS=1
+#export GPU_SINGLE_ALLOC_PERCENT=100
+#export GPU_MAX_ALLOC_PERCENT=100
+#export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+#export GPU_ENABLE_LARGE_ALLOCATION=100
+#export GPU_MAX_WORKGROUP_SIZE=1024
+#!/bin/sh
+reset
+
+sudo ./sreb --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV.colC --password a --cpu-threads 0 
